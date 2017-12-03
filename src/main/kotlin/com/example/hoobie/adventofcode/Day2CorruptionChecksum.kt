914656411cd8767d7ecf093ab1cd764eb809a74e
Hubert Slojewski <hubert.slojewski@tesco.com>
2017-12-03 13:04:35 +0100

    Solve the second part of the 2nd day

--- src/main/kotlin/com/example/hoobie/adventofcode/Day2CorruptionChecksum.kt
+++ src/main/kotlin/com/example/hoobie/adventofcode/Day2CorruptionChecksum.kt
@@ -16,21 +16,51 @@ The first row's largest and smallest values are 9 and 1, and their difference is
 The second row's largest and smallest values are 7 and 3, and their difference is 4.
 The third row's difference is 6.
 In this example, the spreadsheet's checksum would be 8 + 4 + 6 = 18.
+
+--- Part Two ---
+
+"Great work; looks like we're on the right track after all. Here's a star for your effort." However, the program seems a little worried. Can programs be worried?
+
+"Based on what we're seeing, it looks like all the User wanted is some information about the evenly divisible values in the spreadsheet. Unfortunately, none of us are equipped for that kind of calculation - most of us specialize in bitwise operations."
+
+It sounds like the goal is to find the only two numbers in each row where one evenly divides the other - that is, where the result of the division operation is a whole number. They would like you to find those numbers on each line, divide them, and add up each line's result.
+
+For example, given the following spreadsheet:
+
+5 9 2 8
+9 4 7 3
+3 8 6 5
+In the first row, the only two numbers that evenly divide are 8 and 2; the result of this division is 4.
+In the second row, the two numbers are 9 and 3; the result is 3.
+In the third row, the result is 2.
+In this example, the sum of the results would be 4 + 3 + 2 = 9.
  */
 
 object Day2CorruptionChecksum {
 
     fun calculateChecksum(input: String): Int {
-        val diffs: Iterable<Int> = input.split("\n")
-                .map {
-                    println(it)
-                    val ints = it.split("\t").map { it.toInt() }
-                    val min = ints.min() ?: 0
-                    val max = ints.max() ?: 0
-                    max - min
-                }
-        return diffs.sum()
+        return mapAndSum(input, {
+            val ints = splitRow(it)
+            val min = ints.min() ?: 0
+            val max = ints.max() ?: 0
+            max - min
+        })
     }
+
+    fun sumEvenlyDivisibleValues(input: String): Int {
+        return mapAndSum(input, {
+            val ints = splitRow(it)
+            val pair = ints.flatMap { a -> ints.map { b -> Pair(a, b) } }
+                    .find { it.first % it.second == 0 && it.first != it.second } ?: Pair(0, 1)
+            pair.first / pair.second
+        })
+    }
+
+    private fun mapAndSum(input: String, mapper: (c: String) -> Int): Int {
+        return input.split("\n").map(mapper).sum()
+    }
+
+    private fun splitRow(row: String) = row.split("\t").map { it.toInt() }
 
 }
 
@@ -53,4 +83,5 @@ private val input = """414	382	1515	319	83	1327	116	391	101	749	1388	1046	1427	1
 
 fun main(args: Array<String>) {
     println("Checksum: " + Day2CorruptionChecksum.calculateChecksum(input))
+    println("Sum of evenly divisible values: " + Day2CorruptionChecksum.sumEvenlyDivisibleValues(input))
 }
--- src/test/groovy/com/example/hoobie/adventofcode/Day2CorruptionChecksumSpec.groovy
+++ src/test/groovy/com/example/hoobie/adventofcode/Day2CorruptionChecksumSpec.groovy
@@ -13,4 +13,13 @@ class Day2CorruptionChecksumSpec extends Specification {
         "5\t1\t9\t5\n7\t5\t3\n2\t4\t6\t8" || 18
     }
 
+    def "should sum evenly divisible values"() {
+        expect:
+        new Day2CorruptionChecksum().sumEvenlyDivisibleValues(input) == result
+
+        where:
+        input                             || result
+        "5\t9\t2\t8\n9\t4\t7\t3\n3\t8\t6\t5" || 9
+    }
+
 }
