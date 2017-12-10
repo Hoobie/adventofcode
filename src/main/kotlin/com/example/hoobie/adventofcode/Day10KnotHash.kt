91206c54fa7073da1b073f7c44f40566d82bccdc
Hoobie <hoobie92@gmail.com>
2017-12-10 19:33:08 +0100

    Solve the second part of the 10th day

--- src/main/kotlin/com/example/hoobie/adventofcode/Day10KnotHash.kt
+++ src/main/kotlin/com/example/hoobie/adventofcode/Day10KnotHash.kt
@@ -55,22 +55,104 @@ In this example, the first two numbers in the list end up being 3 and 4; to chec
 However, you should instead use the standard list size of 256 (with values 0 to 255)
 and the sequence of lengths in your puzzle input. Once this process is complete,
 what is the result of multiplying the first two numbers in the list?
+
+--- Part Two ---
+
+The logic you've constructed forms a single round of the Knot Hash algorithm; running the full thing requires many of these rounds.
+Some input and output processing is also required.
+
+First, from now on, your input should be taken not as a list of numbers, but as a string of bytes instead. Unless otherwise specified,
+convert characters to bytes using their ASCII codes. This will allow you to handle arbitrary ASCII strings,
+and it also ensures that your input lengths are never larger than 255. For example, if you are given 1,2,3,
+you should convert it to the ASCII codes for each character: 49,44,50,44,51.
+
+Once you have determined the sequence of lengths to use, add the following lengths to the end of the sequence: 17, 31, 73, 47, 23.
+For example, if you are given 1,2,3, your final sequence of lengths should be 49,44,50,44,51,17,31,73,47,23
+(the ASCII codes from the input string combined with the standard length suffix values).
+
+Second, instead of merely running one round like you did above, run a total of 64 rounds,
+using the same length sequence in each round. The current position and skip size should be preserved between rounds.
+For example, if the previous example was your first round, you would start your second round
+with the same length sequence (3, 4, 1, 5, 17, 31, 73, 47, 23, now assuming they came from ASCII codes
+and include the suffix), but start with the previous round's current position (4) and skip size (4).
+
+Once the rounds are complete, you will be left with the numbers from 0 to 255 in some order, called the sparse hash.
+Your next task is to reduce these to a list of only 16 numbers called the dense hash. To do this, use numeric bitwise XOR
+to combine each consecutive block of 16 numbers in the sparse hash (there are 16 such blocks in a list of 256 numbers).
+So, the first element in the dense hash is the first sixteen elements of the sparse hash XOR'd together,
+the second element in the dense hash is the second sixteen elements of the sparse hash XOR'd together, etc.
+
+For example, if the first sixteen elements of your sparse hash are as shown below, and the XOR operator is ^,
+you would calculate the first output number like this:
+
+65 ^ 27 ^ 9 ^ 1 ^ 4 ^ 3 ^ 40 ^ 50 ^ 91 ^ 7 ^ 6 ^ 0 ^ 2 ^ 5 ^ 68 ^ 22 = 64
+
+Perform this operation on each of the sixteen blocks of sixteen numbers in your sparse hash to determine
+the sixteen numbers in your dense hash.
+
+Finally, the standard way to represent a Knot Hash is as a single hexadecimal string;
+the final output is the dense hash in hexadecimal notation.
+Because each number in your dense hash will be between 0 and 255 (inclusive),
+always represent each number as two hexadecimal digits (including a leading zero as necessary).
+So, if your first three numbers are 64, 7, 255, they correspond to the hexadecimal numbers 40, 07, ff,
+and so the first six characters of the hash would be 4007ff. Because every Knot Hash is sixteen such numbers,
+the hexadecimal representation is always 32 hexadecimal digits (0-f) long.
+
+Here are some example hashes:
+
+    The empty string becomes a2582a3a0e66e6e86e3812dcb672a272.
+    AoC 2017 becomes 33efeb34ea91902bb2f59c9920caa6cd.
+    1,2,3 becomes 3efbe78a8d82f29979031a4aa0b16a9d.
+    1,2,4 becomes 63960835bcdc130f0b66d7ff4f6a5a8e.
+
+Treating your puzzle input as a string of ASCII characters, what is the Knot Hash of your puzzle input?
+Ignore any leading or trailing whitespace you might encounter.
  */
 
 object Day10KnotHash {
 
-    fun hashAndMultiplyTheFirstTwoNumbers(input: String, start: Int, end: Int): Int {
-        val list = IntCircularList()
-        list += start..end
+    fun knotHash(input: String): String {
+        val asciiCodes = input.toCharArray().map { it.toInt() }.joinToString(separator = ",")
+        val extraSeparator = if (input.isNotEmpty()) "," else ""
+        val standardLengths = "17,31,73,47,23"
+        val newInput = asciiCodes + extraSeparator + standardLengths
+        val lengths = newInput.split(",").map { it.toInt() }
+
+        var list = IntCircularList()
         var current = 0
         var skip = 0
-        val lengths = input.split(",").map { it.toInt() }
-        for (length in lengths) {
-            current = hash(list, length, current, skip)
-            skip++
+        for (i in 0..63) {
+            val results = hashAndMultiplyTheFirstTwoNumbers(newInput, 0, 255, current = current, skip = skip,
+                    lengths = lengths, list = list)
+            list = results.first
+            current = results.second.first
+            skip = results.second.second
         }
 
-        return list[0] * list[1]
+        return (list as ArrayList<Int>)
+                .chunked(16)
+                .map { it.reduce { acc, i -> acc.xor(i) } }
+                .map { it.toString(16) }.joinToString(separator = "") { it.padStart(2, '0') }
+    }
+
+    fun hashAndMultiplyTheFirstTwoNumbers(input: String, start: Int, end: Int, current: Int = 0, skip: Int = 0,
+                                          list: IntCircularList = IntCircularList(), lengths: List<Int>? = null)
+            : Pair<IntCircularList, Pair<Int, Int>> {
+
+        if (list.isEmpty()) {
+            list += start..end
+        }
+
+        var currCopy = current
+        var skipCopy = skip
+        val lengthsCopy = lengths ?: input.split(",").map { it.toInt() }
+
+        for (length in lengthsCopy) {
+            currCopy = hash(list, length, currCopy, skipCopy)
+            skipCopy++
+        }
+
+        return Pair(list, Pair(currCopy, skipCopy))
     }
 
     private fun hash(list: IntCircularList, length: Int, current: Int, skip: Int): Int {
@@ -105,5 +187,5 @@ class IntCircularList : ArrayList<Int>() {
 private val input = "183,0,31,146,254,240,223,150,2,206,161,1,255,232,199,88"
 
 fun main(args: Array<String>) {
-    println("Result: " + Day10KnotHash.hashAndMultiplyTheFirstTwoNumbers(input, 0, 255))
+    println("Knot hash: " + Day10KnotHash.knotHash(input))
 }
--- src/test/groovy/com/example/hoobie/adventofcode/Day10KnotHashSpec.groovy
+++ src/test/groovy/com/example/hoobie/adventofcode/Day10KnotHashSpec.groovy
@@ -18,11 +18,24 @@ class Day10KnotHashSpec extends Specification {
 
     def "should multiply"() {
         expect:
-        new Day10KnotHash().hashAndMultiplyTheFirstTwoNumbers(input, 0, 4) == result
+        def list = new Day10KnotHash().hashAndMultiplyTheFirstTwoNumbers(input, 0, 4, 0, 0, new IntCircularList(), null).first
+        list[0] * list[1] == result
 
         where:
         input     || result
         "3,4,1,5" || 12
+    }
+
+    def "should hash"() {
+        expect:
+        new Day10KnotHash().knotHash(input) == result
+
+        where:
+        input      || result
+        ""         || "a2582a3a0e66e6e86e3812dcb672a272"
+        "AoC 2017" || "33efeb34ea91902bb2f59c9920caa6cd"
+        "1,2,3"    || "3efbe78a8d82f29979031a4aa0b16a9d"
+        "1,2,4"    || "63960835bcdc130f0b66d7ff4f6a5a8e"
     }
 
 }
