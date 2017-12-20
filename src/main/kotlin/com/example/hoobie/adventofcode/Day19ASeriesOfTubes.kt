9d3d3aa9d7ff8c697c15a8bd2261aa88fb7b0e2f
Hubert Slojewski <hubert.slojewski@tesco.com>
2017-12-20 22:35:15 +0100

    Solve the 19th day

---
+++ src/main/kotlin/com/example/hoobie/adventofcode/Day19ASeriesOfTubes.kt
@@ -0,0 +1,73 @@
+package com.example.hoobie.adventofcode
+
+import com.example.hoobie.adventofcode.utils.FileUtil
+
+/*
+--- Day 19: A Series of Tubes ---
+Somehow, a network packet got lost and ended up here. It's trying to follow a routing diagram (your puzzle input), 
+but it's confused about where to go.
+
+Its starting point is just off the top of the diagram. Lines (drawn with |, -, and +) show the path it needs to take, 
+starting by going down onto the only line connected to the top of the diagram. 
+It needs to follow this path until it reaches the end (located somewhere within the diagram) and stop there.
+
+Sometimes, the lines cross over each other; in these cases, it needs to continue going the same direction, 
+and only turn left or right when there's no other option. In addition, someone has left letters on the line; 
+these also don't change its direction, but it can use them to keep track of where it's been. For example:
+
+     |          
+     |  +--+    
+     A  |  C    
+ F---|----E|--+ 
+     |  |  |  D 
+     +B-+  +--+ 
+
+Given this diagram, the packet needs to take the following path:
+
+Starting at the only line touching the top of the diagram, it must go down, pass through A, and continue onward to the first +.
+Travel right, up, and right, passing through B in the process.
+Continue down (collecting C), right, and up (collecting D).
+Finally, go all the way left through E and stopping at F.
+Following the path to the end, the letters it sees on its path are ABCDEF.
+
+The little packet looks up at you, hoping you can help it find the way. What letters will it see (in the order it would see them) 
+if it follows the path? (The routing diagram is very wide; make sure you view it without line wrapping.)
+ */
+
+object Day19ASeriesOfTubes {
+
+    fun getLettersAndSteps(input: String): Pair<String, Long> {
+        val diagram = input.split("\n").map { it.toCharArray().toList() }
+        val x = diagram[0].indexOf('|')
+        return move("", 1, 0, x, 0, Pair(0, 1), diagram)
+    }
+
+    private tailrec fun move(letters: String, counter: Long, lastLetterStep: Long, x: Int, y: Int, vector: Pair<Int, Int>, diagram: List<List<Char>>): Pair<String, Long> {
+        val current = diagram[y][x]
+        if (y + vector.second < 0 || y + vector.second >= diagram.size
+                || x + vector.first < 0 || x + vector.first >= diagram[y].size)
+            return Pair(letters + current, lastLetterStep)
+
+        if (current == '+' && vector.first == 0 && diagram[y][x - 1] == '-')
+            return move(letters, counter + 1, lastLetterStep, x - 1, y, Pair(-1, 0), diagram)
+        if (current == '+' && vector.first == 0 && diagram[y][x + 1] == '-')
+            return move(letters, counter + 1, lastLetterStep, x + 1, y, Pair(1, 0), diagram)
+        if (current == '+' && vector.second == 0 && diagram[y - 1][x] == '|')
+            return move(letters, counter + 1, lastLetterStep, x, y - 1, Pair(0, -1), diagram)
+        if (current == '+' && vector.second == 0 && diagram[y + 1][x] == '|')
+            return move(letters, counter + 1, lastLetterStep, x, y + 1, Pair(0, 1), diagram)
+
+        if (current.isLetter())
+            return move(letters + current, counter + 1, counter, x + vector.first, y + vector.second, vector, diagram)
+        return move(letters, counter + 1, lastLetterStep, x + vector.first, y + vector.second, vector, diagram)
+    }
+
+}
+
+private val inputFileName = "day19.txt"
+
+fun main(args: Array<String>) {
+    val input = FileUtil.readFile(inputFileName)
+
+    println("Letters and steps: " + Day19ASeriesOfTubes.getLettersAndSteps(input))
+}
