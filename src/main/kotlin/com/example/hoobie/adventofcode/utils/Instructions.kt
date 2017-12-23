355fbb5f08af0e385284883f0583f919f8438064
Hubert Slojewski <hubert.slojewski@tesco.com>
2017-12-23 18:58:31 +0100

    Change instructions

--- src/main/kotlin/com/example/hoobie/adventofcode/utils/Instructions.kt
+++ src/main/kotlin/com/example/hoobie/adventofcode/utils/Instructions.kt
@@ -4,115 +4,115 @@ abstract class Instruction(open val destReg: String) {
     abstract fun eval(registers: Map<String, Long>): Map<String, Long>
 }
 
-data class Sound(val register: String) : Instruction(register) {
+data class Sound(private val register: String) : Instruction(register) {
     override fun eval(registers: Map<String, Long>): Map<String, Long> {
         return registers.plus(Pair("snd", registers[register]!!))
     }
 }
 
-data class SetFromRegister(override val destReg: String, val srcReg: String) : Instruction(destReg) {
+data class SetFromRegister(override val destReg: String, private val srcReg: String) : Instruction(destReg) {
     override fun eval(registers: Map<String, Long>): Map<String, Long> {
         return registers.plus(Pair(destReg, registers[srcReg]!!))
     }
 }
 
-data class Set(val register: String, val value: Long) : Instruction(register) {
+data class Set(private val register: String, val value: Long) : Instruction(register) {
     override fun eval(registers: Map<String, Long>): Map<String, Long> {
         return registers.plus(Pair(register, value))
     }
 }
 
-data class AddRegister(override val destReg: String, val srcReg: String) : Instruction(destReg) {
+data class AddRegister(override val destReg: String, private val srcReg: String) : Instruction(destReg) {
     override fun eval(registers: Map<String, Long>): Map<String, Long> {
         return registers.plus(Pair(destReg, registers[destReg]!! + registers[srcReg]!!))
     }
 }
 
-data class Add(val register: String, val value: Int) : Instruction(register) {
+data class Add(private val register: String, val value: Int) : Instruction(register) {
     override fun eval(registers: Map<String, Long>): Map<String, Long> {
         return registers.plus(Pair(register, registers[register]!! + value))
     }
 }
 
-data class SubRegister(override val destReg: String, val srcReg: String) : Instruction(destReg) {
+data class SubRegister(override val destReg: String, private val srcReg: String) : Instruction(destReg) {
     override fun eval(registers: Map<String, Long>): Map<String, Long> {
         return registers.plus(Pair(destReg, registers[destReg]!! - registers[srcReg]!!))
     }
 }
 
-data class Sub(val register: String, val value: Int) : Instruction(register) {
+data class Sub(private val register: String, val value: Int) : Instruction(register) {
     override fun eval(registers: Map<String, Long>): Map<String, Long> {
         return registers.plus(Pair(register, registers[register]!! - value))
     }
 }
 
-data class MulRegister(override val destReg: String, val srcReg: String) : Instruction(destReg) {
+data class MulRegister(override val destReg: String, private val srcReg: String) : Instruction(destReg) {
     override fun eval(registers: Map<String, Long>): Map<String, Long> {
         return registers.plus(Pair(destReg, registers[destReg]!! * registers[srcReg]!!))
     }
 }
 
-data class Mul(val register: String, val value: Int) : Instruction(register) {
+data class Mul(private val register: String, val value: Int) : Instruction(register) {
     override fun eval(registers: Map<String, Long>): Map<String, Long> {
         return registers.plus(Pair(register, registers[register]!! * value))
     }
 }
 
-data class ModRegister(override val destReg: String, val srcReg: String) : Instruction(destReg) {
+data class ModRegister(override val destReg: String, private val srcReg: String) : Instruction(destReg) {
     override fun eval(registers: Map<String, Long>): Map<String, Long> {
         return registers.plus(Pair(destReg, registers[destReg]!! % registers[srcReg]!!))
     }
 }
 
-data class Mod(val register: String, val value: Int) : Instruction(register) {
+data class Mod(private val register: String, val value: Int) : Instruction(register) {
     override fun eval(registers: Map<String, Long>): Map<String, Long> {
         return registers.plus(Pair(register, registers[register]!! % value))
     }
 }
 
-data class RecoverSound(val register: String) : Instruction(register) {
+data class RecoverSound(private val register: String) : Instruction(register) {
     override fun eval(registers: Map<String, Long>): Map<String, Long> {
         return if (registers[register]!! != 0L) registers.plus(Pair("rcv", registers["snd"]!!)) else registers
     }
 }
 
-data class Jgz(val val1: Long, val val2: Long) : Instruction("jmp") {
+data class Jgz(private val val1: Long, private val val2: Long) : Instruction("jmp") {
     override fun eval(registers: Map<String, Long>): Map<String, Long> {
         return if (val1 > 0) registers.plus(Pair("jmp", val2)) else registers
     }
 }
 
-data class Jrgz(val register: String, val value: Long) : Instruction(register) {
+data class Jrgz(private val register: String, val value: Long) : Instruction(register) {
     override fun eval(registers: Map<String, Long>): Map<String, Long> {
         return if (registers[register]!! > 0) registers.plus(Pair("jmp", value)) else registers
     }
 }
 
-data class Jrgzr(val register: String, val srcReg: String) : Instruction(register) {
+data class Jrgzr(private val register: String, private val srcReg: String) : Instruction(register) {
     override fun eval(registers: Map<String, Long>): Map<String, Long> {
         return if (registers[register]!! > 0) registers.plus(Pair("jmp", registers[srcReg]!!)) else registers
     }
 }
 
-data class Jnz(val val1: Long, val val2: Long) : Instruction("jmp") {
+data class Jnz(private val val1: Long, private val val2: Long) : Instruction("jmp") {
     override fun eval(registers: Map<String, Long>): Map<String, Long> {
-        return if (val1 > 0) registers.plus(Pair("jmp", val2)) else registers
+        return if (val1 != 0L) registers.plus(Pair("jmp", val2)) else registers
     }
 }
 
-data class Jrnz(val register: String, val value: Long) : Instruction(register) {
+data class Jrnz(private val register: String, val value: Long) : Instruction(register) {
     override fun eval(registers: Map<String, Long>): Map<String, Long> {
         return if (registers[register]!! != 0L) registers.plus(Pair("jmp", value)) else registers
     }
 }
 
-data class Jrnzr(val register: String, val srcReg: String) : Instruction(register) {
+data class Jrnzr(private val register: String, private val srcReg: String) : Instruction(register) {
     override fun eval(registers: Map<String, Long>): Map<String, Long> {
         return if (registers[register]!! != 0L) registers.plus(Pair("jmp", registers[srcReg]!!)) else registers
     }
 }
 
-data class SendRegister(val register: String) : Instruction(register) {
+data class SendRegister(private val register: String) : Instruction(register) {
     override fun eval(registers: Map<String, Long>): Map<String, Long> {
         return registers.plus(Pair("snd", registers[register]!!))
     }
@@ -124,7 +124,7 @@ data class Send(val value: Long) : Instruction("snd") {
     }
 }
 
-data class Receive(val register: String) : Instruction(register) {
+data class Receive(private val register: String) : Instruction(register) {
     override fun eval(registers: Map<String, Long>): Map<String, Long> {
         return registers.plus(Pair("rcv", register.toCharArray()[0].toLong())) // hack
     }
